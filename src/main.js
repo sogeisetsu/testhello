c49403482c6eced3c8e3cf35b9efecdfc4d3d0c0
df <df>
2021-04-03 19:40:45 +0800

    对main.js 添加注释

--- src/main.js
+++ src/main.js
@@ -2,10 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import axios from "axios"
-
+// 阻止启动生产消息
 Vue.config.productionTip = false
+// 将axios加入全局的作用域
 Vue.prototype.$axios=axios
 new Vue({
+  // 配置路由
   router,
+  // 将App.vue 渲染进去
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#app')//el #app
