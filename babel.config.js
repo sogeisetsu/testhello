45fb726fcd22b0ae2dacf3a3fc42dad60cafe181
df <df>
2021-04-04 14:02:45 +0800

    按需引入

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,17 @@
 module.exports = {
   presets: [
     '@vue/cli-plugin-babel/preset'
+  ],
+  plugins: [
+    ["import", {
+      "libraryName": "muse-ui",
+      "libraryDirectory": "lib",
+      "camel2DashComponentName": false
+    }],
+    ['import', {
+      libraryName: 'vant',
+      libraryDirectory: 'es',
+      style: true
+    }, 'vant']
   ]
 }
